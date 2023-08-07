077f2441a1baeb9dfd2fe5ed165b9d5f20419cee
Emre Ebeturk <emreebeturk@gmail.com>
2023-08-07 22:18:18 +0200

    implemented Devise, added User model through Devise, used a bootstrap navbar to test js dependencies, imported javascript dependencies using importmaps, updated iimportmap.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  # # routes for users through devise:
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "pages#home"
-
+  root to: 'pages#home'
 end
