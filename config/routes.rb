58f98a381cc306deae26e971a6dd4625feef2bae
Eduardo Roldan Isaac <eduroldanisaac@gmail.com>
2023-09-12 00:13:03 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "pages#home"
+
+  get "home", to: "pages#home"
   get "about", to: "pages#about"
+  get "test", to: "pages#test"
+
 end
